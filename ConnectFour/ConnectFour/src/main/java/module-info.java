060262b8433b191788d2417d04d301f886bcc0ac
Radhe Ravi <90458033+radhe-ravi@users.noreply.github.com>
2025-04-06 00:05:26 +0530

    Delete ConnectFour/ConnectFour directory

--- ConnectFour/ConnectFour/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.connectfour.connectfour {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.connectfour.connectfour to javafx.fxml;
-    exports com.connectfour.connectfour;
-}
